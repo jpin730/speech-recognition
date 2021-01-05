146721c9eb92be693b4de00907205d230cf5fa47
Jaime Pineda <jaime.pineda.hqz@gmail.com>
2021-01-05 16:02:41 -0600

    Adding spanish version.

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
-const text = document.getElementById("text");
+var text = document.getElementById("text");
+var voiceSelect = document.getElementById("voice-select");
+var langSelected = voiceSelect.options[voiceSelect.selectedIndex].getAttribute("data-lang");
 let recognition;
 
 if(!("webkitSpeechRecognition" in window)) {
   alert("Web Kit Speech Recognition API not available.");
 } else {
   recognition = new webkitSpeechRecognition();
-  recognition.lang = "en-US";
+  recognition.lang = langSelected;
   recognition.continuos = true;
   recognition.interim = true;
   // WRITE "text" CALLBACK
@@ -21,10 +23,20 @@ if(!("webkitSpeechRecognition" in window)) {
 document.getElementById("mic").addEventListener("click", () => {
   text.classList.add("font-italic");
   text.innerHTML = "Listening from your mic...";
+  recognition.lang = langSelected;
   recognition.start();
 });
 
 // READ "text"
 document.getElementById("speaker").addEventListener("click", () => {
-  speechSynthesis.speak(new SpeechSynthesisUtterance(text.innerHTML));
+  var reading = new SpeechSynthesisUtterance(text.innerHTML);
+  reading.lang = langSelected;
+  speechSynthesis.speak(reading);
 });
+
+// GET LANG
+function getLang() {
+  voiceSelect = document.getElementById("voice-select");
+  langSelected = voiceSelect.options[voiceSelect.selectedIndex].getAttribute("data-lang");
+}
+voiceSelect.addEventListener("change", getLang);
